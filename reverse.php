ba43870caec901bed550ed635845a296f061915a
ranjithrs <ranjith@Ranjiths-MacBook-Pro.local>
2015-06-16 10:56:03 -0700

    Fixing #CLOUD-1927

--- reverse.php
+++ reverse.php
@@ -8,6 +8,7 @@ echo "Sample string reversal application";
 echo "<br/><br/><br/>";
 echo "The reverse of the input string is : ";
 echo "------------------------------------- ";
+echo "**************************************";
 echo strrev($_GET['input']);
   
   
